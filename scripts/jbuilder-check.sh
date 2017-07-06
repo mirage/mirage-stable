8a50925db1dfc4e5e9044ee3a424239c329a6cec
Anil Madhavapeddy <anil@recoil.org>
2017-07-06 10:34:52 +0100

    clone repos to have a jbuilder-ish environment

--- scripts/jbuilder-check.sh
+++ scripts/jbuilder-check.sh
@@ -5,6 +5,7 @@ for i in `cat $1`; do
   if opam info -f depends: $i |grep -q jbuild; then
     echo $i >> yes-jbuild.txt.pre
     echo $i : yes
+    opam show -f dev-repo: $i >> repos.txt.pre
   else
     echo $i >> no-jbuild.txt.pre
     echo $i : no
@@ -13,3 +14,6 @@ done
 
 sort -u no-jbuild.txt.pre > no-jbuild.txt
 grep -v ^conf- yes-jbuild.txt.pre | grep -v ^base- | grep -v ^jbuild | grep -v ocaml-src | sort -u > yes-jbuild.txt
+cat repos.txt.pre | sed -e 's,git+http:,git:,g' | sed -e 's,git+https:,git:,g' | sort -u > repos.txt
+mkdir repos
+for i in `cat repos.txt`; do git -C repos clone $i; done
